808e20e766e0f1618a7a612bd9514d4649fbf59c
HYK <junhyeok001010@gmail.com>
2025-01-13 23:21:24 +0900

    apply animation to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
+import Header from "@/components/Header";
 import { projectListMock } from "@/mocks";
 import Image from "next/image";
 import Link from "next/link";
 import { GoArrowUpRight } from "react-icons/go";
-import { PiGithubLogoLight, PiInstagramLogoLight } from "react-icons/pi";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles["wrapper"]}>
       <main className={styles["container"]}>
-        <header className={styles["header"]}>
-          <div className={styles["header-left"]}>
-            <h2 className={styles["header-left-pretitle"]}>my finished</h2>
-            <h1 className={styles["header-left-title"]}>projects</h1>
-          </div>
-          <div className={styles["header-right"]}>
-            <div className={styles["header-right-icon-box"]}>
-              <Link href="https://github.com/junhyeokshin" className={styles["header-right-icon"]}>
-                <PiGithubLogoLight />
-              </Link>
-              <Link href="https://instagram.com/junhyeok_shin" className={styles["header-right-icon"]}>
-                <PiInstagramLogoLight />
-              </Link>
-            </div>
-            <div className={styles["header-right-email"]}>junhyeok001010@gmail.com</div>
-          </div>
-        </header>
+        <Header />
         <section className={styles["content"]}>
           {projectListMock.map((projectListItem) => (
             <Link
