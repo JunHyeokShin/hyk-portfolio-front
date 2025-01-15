68aa147c5bd821c1dbfac8cad5e79c2f74262266
HYK <junhyeok001010@gmail.com>
2025-01-15 22:40:04 +0900

    resize menu button

--- src/components/MenuButton/index.tsx
+++ src/components/MenuButton/index.tsx
@@ -24,26 +24,26 @@ export default function MenuButton() {
 
   useGSAP(() => {
     menuToggleAnimation.current = gsap.timeline({ paused: true });
-    menuToggleAnimation.current.to("#menu-button-icon", { top: "-1rem", duration: 0.2, ease: "power2.out" });
+    menuToggleAnimation.current.to("#menu-button-icon", { top: "-1.5rem", duration: 0.2, ease: "power2.out" });
     menuToggleAnimation.current.to("#menu-button-icon-second", { top: 0, duration: 0.2, ease: "power2.out" }, 0);
-    menuToggleAnimation.current.to("#home-button", { visibility: "inherit", y: "-4.125rem", duration: 0.2, ease: "back.out" }, 0);
-    menuToggleAnimation.current.to("#blog-button", { visibility: "inherit", y: "-2.125rem", duration: 0.1, ease: "back.out" }, 0);
+    menuToggleAnimation.current.to("#home-button", { visibility: "inherit", y: "-5.125rem", duration: 0.2, ease: "back.out" }, 0);
+    menuToggleAnimation.current.to("#blog-button", { visibility: "inherit", y: "-2.625rem", duration: 0.1, ease: "back.out" }, 0);
   });
 
   return (
     <div className={styles["container"]}>
-      <button className={styles["menu-button"]} id="menu-button" onClick={onMenuButtonClickHandler}>
-        <div className={styles["menu-button-icon-wrapper"]}>
-          <BsList className={styles["menu-button-icon"]} id="menu-button-icon" />
-          <BsChevronDown className={styles["menu-button-icon-second"]} id="menu-button-icon-second" />
-        </div>
-      </button>
       <Link href="/" className={`${styles["button"]} ${styles["home-button"]}`} id="home-button" onClick={onMenuButtonClickHandler}>
         <TiHome className={styles["button-icon"]} />
       </Link>
       <Link href="/blog" className={`${styles["button"]} ${styles["blog-button"]}`} id="blog-button" onClick={onMenuButtonClickHandler}>
         <RiArticleFill className={styles["button-icon"]} />
       </Link>
+      <button className={styles["menu-button"]} id="menu-button" onClick={onMenuButtonClickHandler}>
+        <div className={styles["menu-button-icon-wrapper"]}>
+          <BsList className={styles["menu-button-icon"]} id="menu-button-icon" />
+          <BsChevronDown className={styles["menu-button-icon-second"]} id="menu-button-icon-second" />
+        </div>
+      </button>
     </div>
   );
 }
