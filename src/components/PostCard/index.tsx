5511b382b7baee3f2c367ea9c79fa8d646a6585e
HYK <junhyeok001010@gmail.com>
2025-03-11 20:25:09 +0900

    renew post card

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -18,8 +18,8 @@ export default function PostCard({ post }: Props) {
   const revealAnimation = useRef<GSAPTimeline>(null);
 
   const onMouseEnterHandler = () => {
-    gsap.to(`#post-${post.id}-thumbnail-wrapper`, { translateY: "1%", scaleY: 1.02, duration: 0.5, ease: "power2.out" });
-    gsap.to(`#post-${post.id}-thumbnail`, { scaleX: 1.1, scaleY: 1.1 / 1.02, duration: 0.5, ease: "power2.out" });
+    gsap.to(`#post-${post.id}-thumbnail-wrapper`, { translateX: "1%", scaleX: 1.02, duration: 0.5, ease: "power2.out" });
+    gsap.to(`#post-${post.id}-thumbnail`, { scaleX: 1.1 / 1.02, scaleY: 1.1, duration: 0.5, ease: "power2.out" });
     gsap.to(`#post-${post.id}-button`, { backgroundColor: "#ffffffff", scale: 1.03, duration: 0.5, ease: "power2.out" });
     gsap.to(`#post-${post.id}-button-text`, { color: "#1a1a1a", duration: 0.5, ease: "power2.out" });
     gsap.to(`#post-${post.id}-button-icon`, { top: "-1.5rem", left: "0", duration: 0.5, ease: "power1.out" });
@@ -27,7 +27,7 @@ export default function PostCard({ post }: Props) {
   };
 
   const onMouseLeaveHandler = () => {
-    gsap.to(`#post-${post.id}-thumbnail-wrapper`, { translateY: 0, scaleY: 1, duration: 0.5, ease: "power2.out" });
+    gsap.to(`#post-${post.id}-thumbnail-wrapper`, { translateX: 0, scaleX: 1, duration: 0.5, ease: "power2.out" });
     gsap.to(`#post-${post.id}-thumbnail`, { scale: 1, duration: 0.5, ease: "power2.out" });
     gsap.to(`#post-${post.id}-button`, { backgroundColor: "#00000000", scale: 1, duration: 0.5, ease: "power2.out" });
     gsap.to(`#post-${post.id}-button-text`, { color: "#ffffff", duration: 0.5, ease: "power2.out" });
@@ -54,20 +54,20 @@ export default function PostCard({ post }: Props) {
       onMouseLeave={onMouseLeaveHandler}
       onTouchEnd={onMouseLeaveHandler}
     >
-      <div className={styles["post-info-box"]}>
-        <div className={styles["post-thumbnail-wrapper"]} id={`post-${post.id}-thumbnail-wrapper`}>
-          <Image
-            src={post.thumbnail ? post.thumbnail : "/resources/default-thumbnail.png"}
-            alt={`${post.id}`}
-            width={1600}
-            height={1200}
-            className={styles["post-thumbnail"]}
-            id={`post-${post.id}-thumbnail`}
-          />
-        </div>
-        <div className={styles["post-info"]}>
+      <div className={styles["post-thumbnail-wrapper"]} id={`post-${post.id}-thumbnail-wrapper`}>
+        <Image
+          src={post.thumbnail ? post.thumbnail : "/resources/default-thumbnail.png"}
+          alt={`${post.id}`}
+          width={1600}
+          height={1200}
+          className={styles["post-thumbnail"]}
+          id={`post-${post.id}-thumbnail`}
+        />
+      </div>
+      <div className={styles["post-info"]}>
+        <div>
           <h2 className={styles["post-title"]}>{post.title}</h2>
-          <ul className={styles["post-tags"]}>
+          <ul>
             {post.tags.map((tag) => (
               <li className={styles["post-tag"]} key={tag}>
                 {tag}
@@ -75,17 +75,18 @@ export default function PostCard({ post }: Props) {
             ))}
           </ul>
         </div>
-      </div>
-      <div className={styles["post-button-container"]}>
-        <button className={styles["post-button"]} id={`post-${post.id}-button`}>
-          <p className={styles["post-button-text"]} id={`post-${post.id}-button-text`}>
-            view post
-          </p>
-          <div className={styles["post-button-icon-wrapper"]}>
-            <GoArrowUpRight className={styles["post-button-icon"]} id={`post-${post.id}-button-icon`} />
-            <GoArrowUpRight className={styles["post-button-icon-second"]} id={`post-${post.id}-button-icon-second`} />
-          </div>
-        </button>
+        <div className={styles["post-info-bottom"]}>
+          <p className={styles["post-write-datetime"]}>{post.createdAt}</p>
+          <button className={styles["post-button"]} id={`post-${post.id}-button`}>
+            <p className={styles["post-button-text"]} id={`post-${post.id}-button-text`}>
+              view post
+            </p>
+            <div className={styles["post-button-icon-wrapper"]}>
+              <GoArrowUpRight className={styles["post-button-icon"]} id={`post-${post.id}-button-icon`} />
+              <GoArrowUpRight className={styles["post-button-icon-second"]} id={`post-${post.id}-button-icon-second`} />
+            </div>
+          </button>
+        </div>
       </div>
     </Link>
   );
